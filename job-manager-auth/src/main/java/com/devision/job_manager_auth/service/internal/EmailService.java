444317c253714105680d48cb45dc65b15f713df7
Phuc Tran <S3924715@rmit.edu.vn>
2025-12-06 17:11:59 +0700

    refactor: Remove Company entity and repository in auth service

--- job-manager-auth/src/main/java/com/devision/job_manager_auth/service/internal/EmailService.java
+++ job-manager-auth/src/main/java/com/devision/job_manager_auth/service/internal/EmailService.java
@@ -1,7 +1,5 @@
 package com.devision.job_manager_auth.service.internal;
 
-import com.devision.job_manager_auth.entity.Company;
-
 // Handle email notification to the company
 public interface EmailService {
     void sendActivationEmail(Company company, String activationToken);
