d6e770443a80a22b4ee60e48e974843b6c5a784a
thai <s3978955@rmit.edu.vn>
2025-12-21 11:18:59 +0700

    added auto generated value for obpost

--- job-manager-jobpost/src/main/java/com/devision/job_manager_jobpost/model/JobPost.java
+++ job-manager-jobpost/src/main/java/com/devision/job_manager_jobpost/model/JobPost.java
@@ -29,6 +29,7 @@ public class JobPost {
     private List<JobPostEmploymentType> employmentTypes = new ArrayList<>();
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "job_post_id")
     private Long jobPostId;
 
