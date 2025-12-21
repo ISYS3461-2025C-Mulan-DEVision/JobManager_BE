916576a5a7d011f1f323bf97ce754ebc9c35b48f
Phuc Tran <S3924715@rmit.edu.vn>
2025-12-21 13:00:56 +0700

    refactor: rename job-manager-search --> job-manager-applicant search,  job-manager-premium --> job-manager-subscription

---
+++ job-manager-subscription/src/main/java/com/devision/job_manager_subscription/JobManagerSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.devision.job_manager_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagerSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagerSubscriptionApplication.class, args);
+	}
+
+}
