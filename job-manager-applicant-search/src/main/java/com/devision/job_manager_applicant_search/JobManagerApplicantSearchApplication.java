798cfb728855b835a332ec75e35e2cd745a64978
Phuc Tran <S3924715@rmit.edu.vn>
2025-12-23 19:44:09 +0700

    feat: enable service discovery in applicant and subscription applications

--- job-manager-applicant-search/src/main/java/com/devision/job_manager_applicant_search/JobManagerApplicantSearchApplication.java
+++ job-manager-applicant-search/src/main/java/com/devision/job_manager_applicant_search/JobManagerApplicantSearchApplication.java
@@ -2,8 +2,10 @@ package com.devision.job_manager_applicant_search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JobManagerApplicantSearchApplication {
 
 	public static void main(String[] args) {
