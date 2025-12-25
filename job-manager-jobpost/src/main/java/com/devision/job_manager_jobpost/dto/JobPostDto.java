0820335c9243f5da44f85e466b6959f99b5c9336
Hanh Do <tranghanh.2702@gmail.com>
2025-12-25 21:03:20 +0700

    Added @JsonProperty annotations to the backend JobPostDto to serialize the fields with the correct names that match the frontend expectations.

--- job-manager-jobpost/src/main/java/com/devision/job_manager_jobpost/dto/JobPostDto.java
+++ job-manager-jobpost/src/main/java/com/devision/job_manager_jobpost/dto/JobPostDto.java
@@ -1,6 +1,7 @@
 package com.devision.job_manager_jobpost.dto;
 
 import com.devision.job_manager_jobpost.model.SalaryType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,15 +19,23 @@ public class JobPostDto {
     private UUID companyId;
     private String title;
     private String description;
+    
+    @JsonProperty("isFresher")
     private boolean fresher;
+    
     private SalaryType salaryType;
     private BigDecimal salaryMin;
     private BigDecimal salaryMax;
     private String salaryNote;
     private String locationCity;
     private String countryCode; // can be derived on client if needed
+    
+    @JsonProperty("isPublished")
     private boolean published;
+    
+    @JsonProperty("isPrivate")
     private boolean aPrivate;
+    
     private LocalDateTime postedAt;
     private LocalDateTime expiryAt;
 }
