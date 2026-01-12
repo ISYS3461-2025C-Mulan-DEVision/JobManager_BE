c5dffe805f81966f3912875d4049e61ada2f2a25
thai <s3978955@rmit.edu.vn>
2026-01-13 01:59:45 +0700

    added: path

--- job-manager-applicant-search/src/main/java/com/devision/job_manager_applicant_search/client/ApplicantClient.java
+++ job-manager-applicant-search/src/main/java/com/devision/job_manager_applicant_search/client/ApplicantClient.java
@@ -56,7 +56,7 @@ public class ApplicantClient {
             int size) {
         try {
             UriComponentsBuilder uriBuilder = UriComponentsBuilder
-                    .fromPath("/api/v1/users/search");
+                    .fromPath("https://be.serverhub.id.vn/api/v1/users/search");
 
             if (skills != null && !skills.isEmpty()) {
                 uriBuilder.queryParam("skills", skills);
